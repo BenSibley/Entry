1b009c4bb05127be1be20f246a1806950e6c56b3
BenSibley <ben@competethemes.com>
2015-08-01 10:38:47 -0400

    started with styles

--- functions.php
+++ functions.php
@@ -1,1 +1,13 @@
-<?php 
+<?php
+
+function ct_entry_load_scripts_styles() {
+
+	wp_register_style( 'ct-entry-google-fonts', '//fonts.googleapis.com/css?family=Open+Sans:300,400,700');
+
+	// dequeue Founder's font to improve performance
+	wp_dequeue_style('ct-founder-google-fonts');
+
+	// enqueue Entry's font
+	wp_enqueue_style('ct-entry-google-fonts');
+}
+add_action('wp_enqueue_scripts', 'ct_entry_load_scripts_styles' );
