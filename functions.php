e851695bbf109dcbb527f86760b857adb549912c
BenSibley <ben@competethemes.com>
2015-08-03 12:52:25 -0400

    lots of style updates

--- functions.php
+++ functions.php
@@ -11,3 +11,15 @@ function ct_entry_load_scripts_styles() {
 	wp_enqueue_style('ct-entry-google-fonts');
 }
 add_action('wp_enqueue_scripts', 'ct_entry_load_scripts_styles' );
+
+function founder_custom_excerpt_length( $length ) {
+
+	$new_excerpt_length = get_theme_mod('excerpt_length');
+
+	// if there is a new length set and it's not 15, change it
+	if( ! empty( $new_excerpt_length ) && $new_excerpt_length != 40 ){
+		return $new_excerpt_length;
+	} else {
+		return 40;
+	}
+}
